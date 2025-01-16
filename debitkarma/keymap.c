af0dcb8f719059a90090348bb4a102cb9ec156b2
Yatri <debitkarma@gmail.com>
2025-01-15 22:49:06 -0500

    restored layer 2 to standard qwerty; changed modifiers (which won't work with F22 macro) to KPASTE, KCOPY, KCUT, and KUNDO

--- debitkarma/keymap.c
+++ debitkarma/keymap.c
@@ -8,6 +8,7 @@ static uint8_t f22_tracker;
 enum custom_keycodes {
     KUNDO = SAFE_RANGE,
     KCOPY,
+    KCUT,
     KPASTE,
     TOGGLEWINLOCK
 };
@@ -28,12 +29,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                  KC_TRNS,KC_NUBS,KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,KC_TRNS, KC_TRNS,          KC_TRNS,                   KC_UP,
                  KC_TRNS,KC_TRNS,KC_TRNS,                KC_TRNS,                                 KC_TRNS,KC_TRNS, KC_TRNS,  KC_TRNS,                                   KC_LEFT, KC_DOWN, KC_RGHT  ),
 [2] = LAYOUT_tkl(
-                 KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11,  KC_F12,             KC_PSCR, KC_SCRL, KC_PAUS,
-                 KC_TRNS, KUNDO,   KCOPY,   KPASTE,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL,  KC_BSPC,   KC_INS,  KC_HOME, KC_PGUP,
-                 KC_TRNS, KC_NO,   KC_NO,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC, KC_BSLS,   KC_DEL,  KC_END,  KC_PGDN,
-                 KC_TRNS, KC_NO,   KC_NO,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,          KC_ENT,
-                 KC_NO,KC_NUBS,KC_NO,   KC_NO,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,            KC_UP,
-                 KC_NO,KC_NO,KC_NO,                KC_SPC,                                 KC_RALT,KC_RGUI, LT(1,KC_APP),  KC_RCTL,   KC_LEFT, KC_DOWN, KC_RGHT  )
+                 KC_ESC,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11,  KC_F12,             KC_PSCR, KC_SCRL, KC_PAUS,
+                 KC_GRV, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL,  KC_BSPC,   KC_INS,  KC_HOME, KC_PGUP,
+                 KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC, KC_BSLS,   KC_DEL,  KC_END,  KC_PGDN,
+                 KC_TRNS, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,          KC_ENT,
+                 KPASTE,KC_NUBS,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,            KC_UP,
+                 KCOPY,KCUT,KUNDO,                KC_SPC,                                 KC_RALT,KC_RGUI, LT(1,KC_APP),  KC_RCTL,   KC_LEFT, KC_DOWN, KC_RGHT  )
 };
 
 void keyboard_pre_init_user(void) {
@@ -85,6 +86,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case KCOPY:
         if (record->event.pressed) {
             SEND_STRING( SS_LCTL(SS_TAP(X_C)) );
+        } else {
+            // when keycode is released
+        }
+        break;
+    case KCUT:
+        if (record->event.pressed) {
+            SEND_STRING( SS_LCTL(SS_TAP(X_X)) );
         } else {
             // when keycode is released
         }
