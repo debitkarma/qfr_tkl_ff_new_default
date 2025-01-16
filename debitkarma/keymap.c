33077472efc6f6f93b9ae79814d1e8d6ee57f32e
Yatri <debitkarma@gmail.com>
2025-01-15 21:30:53 -0500

    changed layer indicator to scrolllock led; added function for toggling win lock and winlock led; added custom keycode for toggling winlock; change KPASTE keycode action for consistency with others

--- debitkarma/keymap.c
+++ debitkarma/keymap.c
@@ -1,9 +1,13 @@
 #include QMK_KEYBOARD_H
 
+// adding tracker for win lock state, initializes to OFF
+static uint8_t winlock_tracker; // = 0;
+
 enum custom_keycodes {
     KUNDO = SAFE_RANGE,
     KCOPY,
     KPASTE,
+    TOGGLEWINLOCK
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -15,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                  KC_LSFT,KC_NUBS,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,            KC_UP,
                  KC_LCTL,KC_LGUI,KC_LALT,                KC_SPC,                                 KC_RALT,KC_RGUI, LT(1,KC_APP),  KC_RCTL,   KC_LEFT, KC_DOWN, KC_RGHT  ),
 [1] = LAYOUT_tkl(
-                 QK_BOOTLOADER, DB_TOGG,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_MPLY,  KC_MSTP,  KC_MPRV,  KC_MNXT,  KC_NO,  KC_MUTE, KC_VOLD,  KC_VOLU,                         KC_TRNS, KC_NUM, KC_TRNS,
+                 QK_BOOTLOADER, DB_TOGG,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_MPLY,  KC_MSTP,  KC_MPRV,  KC_MNXT,  TOGGLEWINLOCK,  KC_MUTE, KC_VOLD,  KC_VOLU,                         KC_TRNS, KC_NUM, KC_TRNS,
                  QK_REBOOT, KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS, KC_TRNS,  KC_TRNS,       KC_TRNS,  KC_TRNS, KC_TRNS,
                  KC_TRNS, KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,  KC_TRNS,  KC_TRNS,
                  KC_CAPS,KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,KC_TRNS,          KC_TRNS,
@@ -36,15 +40,27 @@ void keyboard_pre_init_user(void) {
     setPinOutput(C6);  // initialize C6 for ScrollLock LED
 }
 
+void toggle_windows_lock(void) {
+    if (winlock_tracker) {
+        winlock_tracker++;
+        SS_TAP( QK_MAGIC_GUI_ON );
+        writePinLow(B7);
+    } else {
+        winlock_tracker--;
+        SS_TAP( QK_MAGIC_GUI_OFF );
+        writePinHigh(B7);
+    }
+}
+
 layer_state_t layer_state_set_user(layer_state_t state) {
 
     // indicator LEDs for this board are opposite
     if(IS_LAYER_ON_STATE(state, 2)) {
-        writePinLow(B7);
+        writePinLow(C6);
     } else if(IS_LAYER_ON_STATE(state, 1)) {
-        writePinLow(B7);
+        writePinLow(C6);
     } else {
-        writePinHigh(B7);
+        writePinHigh(C6);
     }
     return state;
 }
@@ -66,11 +82,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         break;
     case KPASTE:
         if (record->event.pressed) {
-            SEND_STRING( SS_LCTL( "v" ) );
+            SEND_STRING( SS_LCTL(X_V) );
         } else {
             // when keycode is released
         }
         break;
+    case TOGGLEWINLOCK:
+        if (record->event.pressed) {
+            toggle_windows_lock();
+        }
     }
     return true;
 };
